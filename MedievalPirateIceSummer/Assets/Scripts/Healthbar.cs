9b6f3d5742826b3f6bcfeec49213fb14e911e0aa
Miguel Velazquez <VelazquezMM@hendrix.edu>
2023-04-29 20:10:22 -0500

    added new components to player and enemy, new scripts(not finished yet)

---
+++ MedievalPirateIceSummer/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void UpdateHealthBar(float maxHealth, float cHealth)
+    {
+
+    }
+}
