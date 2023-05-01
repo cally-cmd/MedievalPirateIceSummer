25cd0830303dea2ccd8210af9b704f1a4501cbfe
Miguel Velazquez <VelazquezMM@hendrix.edu>
2023-04-30 19:34:42 -0500

    major script changes, new layer for scripts

--- MedievalPirateIceSummer/Assets/Scripts/PlayerMovement.cs
+++ MedievalPirateIceSummer/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    //ground check
+    public float playerHeight;
+    public LayerMask WhatIsGround;
+    bool grounded;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
         
+    }
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, WhatIsGround);
+        MyInput();
+
+        if(grounded)
+        {
+            rb.drag = groundDrag;
+        }else{
+            rb.drag = 0;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void movePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
